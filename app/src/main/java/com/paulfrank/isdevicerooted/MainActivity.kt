8abd44a5cf9a0988071e9676490f5e7cdcf299de
UsmanRaoof <usman.rauf@intellicode.tech>
2023-09-04 00:09:59 -0700

    Base project commit

---
+++ app/src/main/java/com/paulfrank/isdevicerooted/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paulfrank.isdevicerooted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
